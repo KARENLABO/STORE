048e025bd506ef861817e920f41232eb4f154d23
karen <karen.bussines@hotmail.com>
2022-12-20 12:15:12 -0600

    Feat create NavBar sticky

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,20 @@
 import './styles.css';
+import Logo from "../../Assets/Logo.png"
 
-
-function NavBar() {
-  return (
-    <div className="App">
-        nav bar
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar--logo-holder">
+      <img src={Logo} alt="logo manu store" className="navbar--logo" />
     </div>
-  );
-}
 
-export default NavBar;
+    <div className="navbar--title">
+     MANU STORE
+    </div>
+
+    <div className="navbar--title">
+      Cart 
+    </div>
+
+  </nav>
+)
+export default Navbar
