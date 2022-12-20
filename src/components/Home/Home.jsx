ed82981ca7c515772b3f0b13b5dc578803a2dde9
karen <karen.bussines@hotmail.com>
2022-12-20 12:55:18 -0600

    Feat: Bring Data from API

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,44 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 
-function App() {
+function Home() {
+  const [data, Setdata] = useState();
+
+
+
+  useEffect(()=>{
+    const bringData = async() => {
+      try{
+        const data = await axios.get('https://fakestoreapi.com/products/');
+        Setdata(data.data);
+      }catch (e) {
+        console.log(e);
+      }
+    }
+
+    bringData();
+  },[]);
+
   return (
-    <div className="App">
-      hola
+    <div className="Home">
+
+      {data && (
+        <>
+        {data.map((item) => {
+          return(
+            <>
+              <p>{item.title}</p>
+              <p> $ {item.price}</p>
+            </>
+          );
+        })}
+        </>
+      )}
     </div>
   );
 }
 
-export default App;
+export default Home;
 
