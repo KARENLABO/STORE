d7ed3642a341b20e0bbe6dd302022c2a6e4188b1
karen <karen.bussines@hotmail.com>
2022-12-22 18:16:58 -0600

    Feat: Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Home from './components/Home/Home'
 import NavBar from './components/NavBar/NavBar';
+import Cart from './components/Cart/Cart'
 import { StoreContextProvider } from "./components/Context/StoreContext";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
+
       <StoreContextProvider>
-        <NavBar/>
-        <Home/>
+        <Router>
+            <NavBar/>
+            <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route exact path="/cart" element={<Cart/>}/>
+            </Routes>
+        </Router>
       </StoreContextProvider>
-    </div>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,29 @@
 import './styles.css';
 import Logo from "../../Assets/Logo.png"
+import {
+  ShoppingCartOutlined
+} from '@ant-design/icons';
+import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav className="navbar">
-    <div className="navbar--logo-holder">
-      <img src={Logo} alt="logo manu store" className="navbar--logo" />
-    </div>
+    <Link to={'/'}>
+      <div className="navbar--logo-holder">
+       <img src={Logo} alt="logo manu store" className="navbar--logo" />
+      </div> 
+    </Link>
+
 
     <div className="navbar--title">
      MANU STORE
     </div>
 
-    <div className="navbar--title">
-      Cart 
+    <div className="navbar--icon">
+      
+    <Link to={'/cart'}>
+      <ShoppingCartOutlined color='white'/>
+    </Link>
+
     </div>
 
   </nav>
